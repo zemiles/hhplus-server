72d88419713aa567b18647cfd53e602adea95e61
안승현 <abeb3@naver.com>
2025-12-28 21:57:27 +0900

    좌석 예약 스케줄링 해제 구현

--- src/main/java/kr/hhplus/be/server/reservation/repository/ReservationJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/reservation/repository/ReservationJpaRepository.java
@@ -3,10 +3,13 @@ package kr.hhplus.be.server.reservation.repository;
 import kr.hhplus.be.server.reservation.domain.Reservation;
 import kr.hhplus.be.server.reservation.domain.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +18,18 @@ public interface ReservationJpaRepository extends JpaRepository<Reservation, Lon
 	
 	@Query("SELECT COUNT(r) > 0 FROM Reservation r WHERE r.seat.seatId = :seatId AND r.status = :status")
 	boolean existsBySeatIdAndStatus(@Param("seatId") Long seatId, @Param("status") ReservationStatus status);
+
+	@Query("SELECT r FROM Reservation r " +
+		"WHERE r.status = :status AND r.holdExpiresAt < :now")
+	List<Reservation> findExpiredReservations(
+			@Param("status") ReservationStatus status,
+			@Param("now")LocalDateTime now
+			);
+
+	@Modifying
+	@Query("UPDATE Reservation r SET r.status = :newStatus" +
+		"WHERE r.status = :oldStatus AND r.holdExpiresAt < :now")
+		int expireReservations(@Param("oldStatus") ReservationStatus oldStatus,
+		                       @Param("newStatus") ReservationStatus newStatus,
+		                       @Param("now") LocalDateTime now);
 }
